3e1222d26493044e56b298d3994fb1037e9e5223
Hectorsxo <hectorcrebu@gmail.com>
2025-09-19 17:44:40 +0200

    Creación de constructores por defecto, vacio y con parametros Creación de setter y getters, Creacion de metodo toString y comentarios de cada elemeto

--- src/main/java/com/ieschabas/Persona.java
+++ src/main/java/com/ieschabas/Persona.java
@@ -1,4 +1,151 @@
 package com.ieschabas;
 
 public class Persona {
+    private int edad;
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+
+    //Constructor por defecto
+   /* public Persona () {
+        this.edad = 15;
+        this.dni = "Y84290426";
+        this.nombre = "Alex";
+        this.apellidos = "Parry";
+        this.telefono = "+34623578043";
+
+    }
+    */
+
+
+    /**
+     * Constructor por defecto crrea una instancia de persona sin datos
+     */
+    public Persona ()  {
+
+    }
+
+
+    /**
+     * Constructor sobre cargado de la clase persona, crea una instancia con los datos pasados como parametros
+     * @param edad
+     * @param dni
+     * @param nombre
+     * @param apellidos
+     * @param telefono
+     */
+
+    public Persona(int edad, String dni, String nombre, String apellidos, String telefono) {
+        this.edad = edad;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+    /**
+     * Inserta la edad en el objeto personas
+     * @param edad
+     */
+
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * Inserta el Dni en el objeto persona
+     * @param dni
+     */
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * Inserta el nombre en el objeto persona
+     * @param nombre
+     */
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Inserta el apellido en el objeto persona
+     * @param apellidos
+     */
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     *Inserta el telefono en el objeto persona
+     * @param telefono
+     */
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * Devuelve la edad del objeto persona
+     * @return
+     */
+
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Devuelve el dni del objeto persona
+     * @return
+     */
+
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * Devuelve el nombre del objeto persona
+     * @return
+     */
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Devuelve el apellidos del objeto persona
+     * @return
+     */
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * Devuelve el telefono del objeto persona
+     * @return
+     */
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * Devuelve el objeto persona en formato cadena
+     * @return cadena con los valores de persona
+     */
+
+    //Metodo toString
+
+    public String toString(){
+        return "Nombre:" + this.nombre + "\n apellidos " + this.apellidos + "\n edad: " + this.edad + "\n dni: " + this.dni + "\n telefono: " + this.telefono;
+
+
+
+    }
 }
