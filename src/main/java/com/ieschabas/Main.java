5318af16623496a8dd129fa298bff6c47c7e0217
Hectorsxo <hectorcrebu@gmail.com>
2025-09-23 21:10:17 +0200

    Creada clase PotenciaYRaiz y su imstancia en Main. Adición de los comentarios faltantes en la clase Main.

--- src/main/java/com/ieschabas/Main.java
+++ src/main/java/com/ieschabas/Main.java
@@ -2,53 +2,37 @@ package com.ieschabas;
 
 public class Main {
     public static void main(String[] args) {
-        //objeto Persona
+        /**
+         * Uso de la clase Persona.
+         */
         Persona persona1 = new Persona();
 
-        /**
-         * Asignacion de atributos al objeto persona1
-         */
+        //Asignacion de atributos al objeto persona1
         persona1.setNombre(" Héctor");
         persona1.setApellidos("Crespo Buigues");
         persona1.setEdad(18);
         persona1.setDni("1020393989B");
         persona1.setTelefono("+34695683459");
 
-        /**
-         * Imprimir persona1
-         */
-
-
+        //Imprimir persona1
         System.out.println(persona1.toString());
 
-        /**
-         * Creacion de persona2 con sus parametros
-         */
 
+        //Creacion de persona2 con sus parametros
         Persona persona2 = new Persona(18,"1020393989B","Héctor","Crespo Buigues","+34695683459");
 
-        /**
-         * Cambiar edad de persona1
-         */
-
+        //Cambiar edad y nombre de persona1
         persona1.setEdad(50);
-
-        /**
-         *Cambiamos nombre de persona 1
-         */
-
-
         persona1.setNombre("Jesus");
 
-        /**
-         * Imprimir persona1 modificado y persona2
-         */
-
+        //Imprimir persona1 modificado y persona2
         System.out.println(persona1.toString());
-
         System.out.println(persona2.toString());
 
-        //Uso de la clase Rectangulo
+
+        /**
+         * Uso de la clase Rectangulo
+         */
         Rectangulo rectangulo1 = new Rectangulo();
         Rectangulo rectangulo2 = new Rectangulo(2.4, 2.7);
 
@@ -56,20 +40,27 @@ public class Main {
         rectangulo1.setBase(5.2);
         rectangulo1.setAltura(6.2);
 
+        //Muestra por pantalla ambos rectángulos
         System.out.println("Área del rectángulo 1: " + rectangulo1.calcularArea());
         System.out.println("Área del rectángulo 2: " + rectangulo2.calcularArea());
 
 
-        //Uso de la clase cuentaBancaria
+        /**
+         * Uso de la clase cuentaBancaria.
+         */
         cuentaBancaria cuenta = new cuentaBancaria(1000, "Carlos");
+        //Usamos los métodos ingresar y retira
         cuenta.ingresar(500);
         cuenta.retirar(300);
+        //Muestra en pantalla el saldo actual
         System.out.println("Saldo actual: " + cuenta.getSaldo());
 
 
-        //Uso de la clase coche
+        /**
+         * Uso de la clase Coche
+         */
 
-        // Crear un coche con velocidad inicial de 50Coche
+        // Crear un coche con velocidad inicial de 50
         Coche coche = new Coche(50);
 
         // Mostrar la velocidad inicial
@@ -88,6 +79,16 @@ public class Main {
         System.out.println("Velocidad después de frenar demasiado: " + coche.getVelocidad() + " km/h");
 
 
+        /**
+         * uso de la clase PotenciaYRaiz
+         */
 
+        // Se crea un objeto de la clase PotenciaYRaiz para calcular la raíz cuadrada
+        PotenciaYRaiz raiz1 = new PotenciaYRaiz();
+        raiz1.raiz();// Se llama al método que calcula y muestra la raíz cuadrada.
+
+        // Se crea otro objeto de la clase PotenciaYRaiz para calcular la potencia
+        PotenciaYRaiz potencia1 = new PotenciaYRaiz();
+        potencia1.potencia();// Se llama al método que calcula y muestra la potencia.
     }
 }
